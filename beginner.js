d467b9fa5fc94ae6f0257df22f4b5ed915ec781f
Victor <onetworacks@gmail.com>
2024-12-03 17:34:04 +0000

    Return the function of two numbers

---
+++ beginner.js
@@ -0,0 +1,11 @@
+/** QUESTION NUMBER 1
+ * Return the function of two numbers
+ * Create a function that takes two arguments as functions and return their sum
+ * addition (3,2) -> 5
+ * 
+ */
+
+function addition(num1, num2) {
+    return num1 + num2
+}
+console.log(addition(3, 5));
